b001024fad470bab8785ed0bb9047ae7d557e652
Orest <orest-2@yandex.ua>
2019-05-17 12:21:49 +0300

    finish lab work 5

--- app/scripts/lab_work_5.py
+++ app/scripts/lab_work_5.py
@@ -3,10 +3,12 @@ import statistics
 
 import numpy as np
 from bokeh.util.string import encode_utf8
+from bokeh.resources import INLINE
+from bokeh.embed import components
 from flask import redirect, render_template, request, url_for
 
 from app import app
-from app.scripts.helpers import to_int_if_can
+from app.scripts.helpers import to_int_if_can, get_figure
 
 
 def get_selective_averages(axis_arr, sum_arr, N, n=1):
@@ -56,7 +58,7 @@ def lab_work_5_post():
             res += v * y[index]
 
         averages_y_for_x.append(
-            to_int_if_can(round(res/sum_rows[i], 2))
+            to_int_if_can(round(res/sum_rows[i], 3))
         )
         res = 0
 
@@ -67,16 +69,78 @@ def lab_work_5_post():
             res += v * x[index]
 
         averages_x_for_y.append(
-            to_int_if_can(round(res/sum_cols[j], 2))
+            to_int_if_can(round(res/sum_cols[j], 3))
         )
         res = 0
 
     selective_averages_x = get_selective_averages(x, sum_rows, sum_n)
+    selective_averages_x_2 = get_selective_averages(x, sum_rows, sum_n, 2)
+    sigma_x = math.sqrt(
+        selective_averages_x_2 - math.pow(selective_averages_x, 2)
+    )
+
     selective_averages_y = get_selective_averages(y, sum_cols, sum_n)
+    selective_averages_y_2 = get_selective_averages(y, sum_cols, sum_n, 2)
+    sigma_y = math.sqrt(
+        selective_averages_y_2 - math.pow(selective_averages_y, 2)
+    )
+
+    sum_nxy_x_y = 0
+
+    for i in range(0, x_size-1):
+        for index, v in enumerate(matrix[i]):
+            if v == 0:
+                continue
+            sum_nxy_x_y += v * y[index] * x[i]
+
+    selective_correlation_coefficient = (
+        sum_nxy_x_y - sum_n *
+        selective_averages_x *
+        selective_averages_y
+    ) / (
+        sum_n *
+        sigma_x *
+        sigma_y
+    )
+
+    f_x_1 = selective_correlation_coefficient * sigma_y / sigma_x
+    # f_x_1 = selective_correlation_coefficient * sigma_x / sigma_y
+    f_x_2 = selective_averages_y + ((selective_averages_x * -1) * f_x_1)
+    # f_x_2 = selective_averages_x + (selective_averages_y * f_x_1)
+
+    distr_conditional_averages_x = []
+    distr_conditional_averages_y = []
+
+    for e in x:
+        distr_conditional_averages_x.append({
+            'x': e,
+            'y': round(f_x_1*e+f_x_2, 3)
+        })
+
+    # for e in y:
+    #     distr_conditional_averages_y.append({
+    #         'y': e,
+    #         'x': round((f_x_2+e)/(f_x_1*-1), 3)
+    #     })
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    fig = get_figure(x, averages_y_for_x, 'x', 'y')
+    plot_1 = {}
+    plot_1['script'], plot_1['div'] = components(fig)
+
+    fig2 = get_figure(y, averages_x_for_y, 'x', 'y')
+    plot_2 = {}
+    plot_2['script'], plot_2['div'] = components(fig2)
 
     html = render_template(
         'lab_work_5/lab_work_5_res.html',
         action_url='/lab_work_5',
+        js_resources=js_resources,
+        css_resources=css_resources,
+        plot_1=plot_1,
+        plot_2=plot_2,
         x=x,
         y=y,
         matrix=matrix,
@@ -86,7 +150,16 @@ def lab_work_5_post():
         averages_y_for_x=averages_y_for_x,
         averages_x_for_y=averages_x_for_y,
         selective_averages_x=selective_averages_x,
-        selective_averages_y=selective_averages_y
+        selective_averages_y=selective_averages_y,
+        sigma_x=round(sigma_x, 3),
+        sigma_y=round(sigma_y, 3),
+        selective_correlation_coefficient=round(
+            selective_correlation_coefficient, 3
+        ),
+        f_x_1=round(f_x_1, 3),
+        f_x_2=round(f_x_2, 3),
+        distr_conditional_averages_x=distr_conditional_averages_x,
+        distr_conditional_averages_y=distr_conditional_averages_y
     )
     return encode_utf8(html)
 
